2c881ad9e047e8b577047a908a4bb1bfa73c3770
moriyamato-win <flactly@gmail.com>
2024-04-25 01:01:28 +0900

    deploy update viteconfigjs

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,19 @@ import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue(), VueDevTools()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   },
+//   build: {
+//     outDir: './docs'
+//   }
+// })
 export default defineConfig({
+  base: '/moriyamato-pf/',
   plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
