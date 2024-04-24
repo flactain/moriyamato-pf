1fbaa8861ca17515f7e62c9c6498bb6d448e55a2
moriyamato-win <flactly@gmail.com>
2024-04-25 07:58:19 +0900

    a

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref, computed } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import TodoCard from './views/TodoCard.vue'
 const todos = ref([
   { title: 'ご飯を食べる🍚', limit: '2024-04-15', done: false },
@@ -20,19 +22,36 @@ const doneTodo = computed(() => {
 </script>
 
 <template>
-  <h1>ToDo</h1>
-  <TodoCard
-    v-for="(todo, index) in doingTodo"
-    :key="todo.title"
-    :todo="todo"
-    @click="todo.done = !todo.done"
-    @delete-todo="todos.splice(index, 1)"
-  />
-  <h1>完了</h1>
-  <TodoCard
-    v-for="todo in doneTodo"
-    :key="todo.title"
-    :todo="todo"
-    @click="todo.done = !todo.done"
-  />
+  <div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a
+        href="/"
+        class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+      >
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap" /></svg>
+        <span class="fs-4">ToDo Application</span>
+      </a>
+    </header>
+  </div>
+
+  <div class="container">
+    <h1 class="display-5" style="text-align: center">ToDo</h1>
+    <TodoCard
+      v-for="(todo, index) in doingTodo"
+      :key="todo.title"
+      :todo="todo"
+      @click="todo.done = !todo.done"
+      @delete-todo="todos.splice(index, 1)"
+    />
+  </div>
+
+  <div class="container">
+    <h1 class="display-5" style="text-align: center">Done</h1>
+    <TodoCard
+      v-for="todo in doneTodo"
+      :key="todo.title"
+      :todo="todo"
+      @click="todo.done = !todo.done"
+    />
+  </div>
 </template>
--- src/views/TodoCard.vue
+++ src/views/TodoCard.vue
@@ -5,9 +5,32 @@ const props = defineProps({
 </script>
 
 <template>
-  <li>
-    <button @click="$emit('delete-todo')">削除</button>
-    <span>{{ props.todo.title }}</span
-    ><span>{{ props.todo.limit }}</span>
-  </li>
+  <div class="row btn-light mori-todo-card">
+    <button
+      type="button "
+      @click="$emit('delete-todo')"
+      class="col-2 btn-sm btn-secondary mori-button"
+    >
+      削除
+    </button>
+    <span class="col-7" style="margin: auto">{{ props.todo.title }}</span>
+    <span class="badge bg-primary mori-badge"> {{ props.todo.limit }}</span>
+  </div>
 </template>
+
+<style scoped>
+.mori-badge {
+  margin: auto;
+}
+
+.mori-todo-card {
+  padding: 1px;
+  margin: 2px;
+  border-radius: 10px;
+  font-size: small;
+}
+.mori-button {
+  padding: 0.2em 0.4em;
+  border-radius: 10px;
+}
+</style>
